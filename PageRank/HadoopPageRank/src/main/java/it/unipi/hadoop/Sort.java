28730909760d5c4bb209c231fc0a832cc35b57eb
ahmed531998 <37671912+ahmed531998@users.noreply.github.com>
2021-07-19 01:34:56 +0200

    Update Sort.java

--- PageRank/HadoopPageRank/src/main/java/it/unipi/hadoop/Sort.java
+++ PageRank/HadoopPageRank/src/main/java/it/unipi/hadoop/Sort.java
@@ -5,12 +5,15 @@ import it.unipi.hadoop.hadoopobjects.Page;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
 
 import java.io.IOException;
 
