9fd9817fcacbe96e338fcf179aeb186265d76ffe
ahmed531998 <ahmed531998@aucegypt.edu>
2021-07-18 11:47:59 +0200

    Load Balancer Added

---
+++ CephProject/LB.py
@@ -0,0 +1,32 @@
+#!flask/bin/python
+from flask import Flask, request, redirect
+import time
+
+app = Flask(__name__)
+
+IPs = 'monitors.txt'
+
+@app.route('/files', methods=['GET', 'POST'])
+def fwdUploadLS():
+    myIP = chooseIP()  
+    return redirect('http://{}:8080/{}'.format(myIP, request.path), code=307)
+
+@app.route('/stats', methods=['GET'])
+def stats():
+    myIP = chooseIP()  
+    return redirect('http://{}:8080/{}'.format(myIP, request.path), code=307)
+
+@app.route('/files/<name>', methods=['GET', 'DELETE'])
+def fwdDeleteDownload(name):
+    myIP = chooseIP()  
+    return redirect('http://{}:8080/{}'.format(myIP, request.path), code=307)
+
+def chooseIP():
+    lines = []
+    with open(IPs) as f:
+        lines = f.readlines()
+    now = time.time()
+    return lines[hash(now)%len(lines)]
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
