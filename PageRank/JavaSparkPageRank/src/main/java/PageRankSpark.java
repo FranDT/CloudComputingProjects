36ba580e61f50c0278884a095e5d12382b804d3d
FranDT <73546916+FranDT@users.noreply.github.com>
2021-07-19 17:47:16 +0200

    Spark PageRank in Java.

--- PageRank/JavaSparkPageRank/src/main/java/PageRankSpark.java
+++ PageRank/JavaSparkPageRank/src/main/java/PageRankSpark.java
@@ -15,23 +15,23 @@ import java.util.regex.Pattern;
 public class PageRankSpark {
 
     public static void main(String[] args) {
-        if (args.length != 4) {
+        if (args.length != 5) {
             System.out.println("Error: wrong number of arguments. Please insert in order the path of the input file,\n" +
                     "the path for the outputs, the number of iterations and the value for alpha");
             System.exit(1);
         }
 
-        final String INPUT_PATH = args[0];
-        final String OUTPUT_PATH = args[1];
-        final int NUM_ITER = Integer.parseInt(args[2]);
-        final double ALPHA = Double.parseDouble(args[3]);
+        final String INPUT_PATH = args[1];
+        final String OUTPUT_PATH = args[2];
+        final int NUM_ITER = Integer.parseInt(args[3]);
+        final double ALPHA = Double.parseDouble(args[4]);
 
         final SparkConf sparkConf = new SparkConf().setMaster("yarn").setAppName("PageRank");
         final JavaSparkContext sc = new JavaSparkContext(sparkConf);
 
         final JavaRDD<String> pages = sc.textFile(INPUT_PATH).cache();
 
-        System.out.println("\n\n\n\n\nProva 1");
+        System.out.println("\n\n\n\n\n" + args[0]);
 
         /**
          *
@@ -42,50 +42,46 @@ public class PageRankSpark {
          *
          *
          */
-        JavaPairRDD<String, Iterable<String>> all_nodes = pages.flatMapToPair(new PairFlatMapFunction<String, String, Iterable<String>>() {
-            public Iterator<Tuple2<String, Iterable<String>>> call(String page) throws Exception {
-                System.out.println("\n\n\n\n\nProva 2");
-                String[] arr;
-                String title = "";
-                Matcher matcher;
-                List<String> outlinks = new LinkedList<String>();
-                List<Tuple2<String, Iterable<String>>> myRDD = new ArrayList<Tuple2<String, Iterable<String>>>();
-                while (page != null) {
-                    Pattern pattern = Pattern.compile("<title.*?>(.*?)</title>");
-                    matcher = pattern.matcher(page);
+        JavaPairRDD<String, Iterable<String>> all_nodes = pages.flatMapToPair((PairFlatMapFunction<String, String, Iterable<String>>) page -> {
+            System.out.println("\n\n\n\n\nProva 2");
+            String[] arr;
+            String title = "";
+            Matcher matcher;
+            List<String> outlinks = new LinkedList<>();
+            List<Tuple2<String, Iterable<String>>> myRDD = new ArrayList<>();
+            while (page != null) {
+                Pattern pattern = Pattern.compile("<title.*?>(.*?)</title>");
+                matcher = pattern.matcher(page);
 
-                    if (matcher.find()) {
-                        title = matcher.group(1).replace("\t", "");
-                    }
-                    arr = page.split("\\[\\[");
-                    for (int i = 0; i < arr.length; i++) {
-                        if (arr[i].contains("]]") && !outlinks.contains(arr[i].substring(0, arr[i].indexOf("]]")))) {
-                            outlinks.add(arr[i].substring(0, arr[i].indexOf("]]")));
-                            myRDD.add(new Tuple2<String, Iterable<String>>(arr[i].substring(0, arr[i].indexOf("]]")), new ArrayList<String>()));
-                        }
-                    }
-                    myRDD.add(new Tuple2<String, Iterable<String>>(title, outlinks));
+                if (matcher.find()) {
+                    title = matcher.group(1).replace("\t", "");
                 }
-                return myRDD.iterator();
+                arr = page.split("\\[\\[");
+                for (int i = 0; i < arr.length; i++) {
+                    if (arr[i].contains("]]") && !outlinks.contains(arr[i].substring(0, arr[i].indexOf("]]")))) {
+                        outlinks.add(arr[i].substring(0, arr[i].indexOf("]]")));
+                        myRDD.add(new Tuple2<>(arr[i].substring(0, arr[i].indexOf("]]")), new ArrayList<String>()));
+                    }
+                }
+                myRDD.add(new Tuple2<>(title, outlinks));
             }
+            return myRDD.iterator();
         });
 
         System.out.println("\n\n\n\nProva 3");
-        JavaPairRDD<String, Iterable<String>> unique_nodes = all_nodes.reduceByKey(new Function2<Iterable<String>, Iterable<String>, Iterable<String>>() {
-            public Iterable<String> call(Iterable<String> outlinks1, Iterable<String> outlinks2) throws Exception {
-                System.out.println("\n\n\n\n\nProva 4");
-                List<String>  ret = new ArrayList<String>();
+        JavaPairRDD<String, Iterable<String>> unique_nodes = all_nodes.reduceByKey((Function2<Iterable<String>, Iterable<String>, Iterable<String>>) (outlinks1, outlinks2) -> {
+            System.out.println("\n\n\n\n\nProva 4");
+            List<String>  ret = new ArrayList<String>();
 
-                for(String link : outlinks1){
-                    ret.add(link);
-                }
-
-                for(String link : outlinks2){
-                    ret.add(link);
-                }
-
-                return ret;
+            for(String link : outlinks1){
+                ret.add(link);
             }
+
+            for(String link : outlinks2){
+                ret.add(link);
+            }
+
+            return ret;
         }).cache();
 
         /**
@@ -102,12 +98,8 @@ public class PageRankSpark {
         */
         JavaRDD<String> nodes_titles = unique_nodes.keys().cache();
 
-        JavaPairRDD<String, Double> titles_and_masses = nodes_titles.mapToPair(new PairFunction<String, String, Double>() {
-
-            public Tuple2<String, Double> call(String title) throws Exception {
-                return new Tuple2<String, Double>(title, 1.0/pageNumber);
-            }
-        });
+        JavaPairRDD<String, Double> titles_and_masses = nodes_titles.mapToPair((PairFunction<String, String, Double>)
+                title -> new Tuple2<>(title, 1.0/pageNumber));
 
         /**
          *
@@ -126,37 +118,30 @@ public class PageRankSpark {
 
             final JavaRDD<Tuple2<Iterable<String>, Double>> outlink_nodes_with_mass = complete_nodes.values();
 
-            JavaPairRDD<String, Double> outlink_masses = outlink_nodes_with_mass.flatMapToPair(new PairFlatMapFunction<Tuple2<Iterable<String>, Double>, String, Double>() {
-                public Iterator<Tuple2<String, Double>> call(Tuple2<Iterable<String>, Double> myTuple) throws Exception {
+            JavaPairRDD<String, Double> outlink_masses = outlink_nodes_with_mass.flatMapToPair((PairFlatMapFunction<Tuple2<Iterable<String>, Double>, String, Double>)
+                    myTuple -> {
 
-                    List<Tuple2<String, Double>> ret = new ArrayList<Tuple2<String, Double>>();
-                    Double node_mass = myTuple._2;
-                    List<String> outlinks_list = new ArrayList<String>();
-                    for(String outlink : myTuple._1){
-                        outlinks_list.add(outlink);
-                    }
+                List<Tuple2<String, Double>> ret = new ArrayList<>();
+                Double node_mass = myTuple._2;
+                List<String> outlinks_list = new ArrayList<>();
+                for(String outlink : myTuple._1){
+                    outlinks_list.add(outlink);
+                }
 
-                    if(outlinks_list.isEmpty())
-                        return ret.iterator();
-                    else{
-                        for(String pageToOutput : outlinks_list)
-                            ret.add(new Tuple2<String, Double>(pageToOutput, node_mass/outlinks_list.size()));
-                    }
+                if(outlinks_list.isEmpty())
                     return ret.iterator();
+                else{
+                    for(String pageToOutput : outlinks_list)
+                        ret.add(new Tuple2<>(pageToOutput, node_mass/outlinks_list.size()));
                 }
+                return ret.iterator();
             });
 
-            JavaPairRDD<String, Double> outlink_total_values = outlink_masses.reduceByKey(new Function2<Double, Double, Double>() {
-                public Double call(Double contribution1, Double contribution2){
-                    return contribution1 + contribution2;
-                }
-            });
+            JavaPairRDD<String, Double> outlink_total_values = outlink_masses.reduceByKey((Function2<Double, Double, Double>)
+                    (contribution1, contribution2) -> contribution1 + contribution2);
 
-            titles_and_masses = outlink_total_values.mapValues(new Function<Double, Double>() {
-                public Double call(Double nodeTotalValue) throws Exception {
-                    return ALPHA/pageNumber + (1-ALPHA)* nodeTotalValue;
-                }
-            });
+            titles_and_masses = outlink_total_values.mapValues((Function<Double, Double>)
+                    nodeTotalValue -> ALPHA/pageNumber + (1-ALPHA)* nodeTotalValue);
         }
 
         /**
