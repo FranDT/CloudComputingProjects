e7952a98140e3e74fdc9691eaf183beb7f5bbcc8
ahmed531998 <ahmed531998@aucegypt.edu>
2021-07-20 18:42:01 +0200

    pr

--- PageRank/HadoopPageRank/src/main/java/it/unipi/hadoop/Parse.java
+++ PageRank/HadoopPageRank/src/main/java/it/unipi/hadoop/Parse.java
@@ -13,7 +13,7 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 
 
 import java.io.IOException;
--- PageRank/HadoopPageRank/src/main/java/it/unipi/hadoop/hadoopobjects/Page.java
+++ PageRank/HadoopPageRank/src/main/java/it/unipi/hadoop/hadoopobjects/Page.java
@@ -87,77 +87,7 @@ public class Page implements WritableComparable<Page> {
         String thatTitle = that.getTitle();
         return this.rank < thatRank ? 1 : (this.rank == thatRank ? this.title.compareTo(thatTitle) : -1);
     }
-    private static double pageRank;
-    private static String title;
-
-    public Page(){
-        pageRank = 0;
-        title = "";
-    }
-
-    public void set(String title, double pageRank){
-        setTitle(title);
-        setPageRank(pageRank);
-    }
-
-    public void setTitle(String title){
-        this.title = title;
-    }
-
-    public void setPageRank(double pageRank){
-        this.pageRank = pageRank;
-    }
-
-    public String getTitle(){
-        return title;
-    }
-
-    public double getPageRank(){
-        return pageRank;
-    }
-
-    public void write(DataOutput out) throws IOException {
-        out.writeUTF(title);
-        out.writeDouble(pageRank);
-    }
-
-    public void readFields(DataInput in) throws IOException {
-        title = in.readUTF();
-        pageRank = in.readDouble();
-    }
     
-    @Override
-    public int hashCode() {
-        return this.title.hashCode() + new Double(this.pageRank).hashCode();
-    }
-    
-    
-    @Override
-    public boolean equals(Object obj) {
-        if(obj instanceof Page){
-            Page o = (Page)obj;
-            return this.title.equals(o.getTitle()) && this.pageRank == o.getPageRank();
-        }
-        return false;
-    }
-    
-    @Override
-    public String toString() {
-        return "Title:" + this.title + "\tRank:" + this.pageRank;
-    }
-
-    public int compareTo(Page o) {
-        double mis = (this.pageRank - o.getPageRank());
-        if(mis > 0 ){
-            return 1;
-        } else if (mis < 0){
-            return -1;   
-        }
-        else{
-            return this.getTitle().compareTo(o.getTitle());
-        }
-    }
-
    /* public int compareTo(Page target) {
         double rank = target.getPageRank();
         String title = target.getTitle();
